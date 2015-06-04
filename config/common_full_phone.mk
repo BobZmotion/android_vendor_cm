daaecd9e952e4a8a75f06d983f64e2ca20052708
Michael Bestas <mikeioannina@gmail.com>
2015-06-04 21:42:46 +0300

    Move LatinIME to phone/tablet configs
    * Move LatinIME & dictionary overlay to exclude it from TV devices
    
    Change-Id: I926fe0a2c24ee3d2c807e1f584a37d57f1cbb8b5

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,6 +1,13 @@
 # Inherit common CM stuff
 $(call inherit-product, vendor/cm/config/common_full.mk)
 
+# Required CM packages
+PRODUCT_PACKAGES += \
+    LatinIME
+
+# Include CM LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
+
 # Default notification/alarm sounds
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=somebodys.ogg \
